db6d05088fa45f877a5cb73ac7d0878d91419e20
haoyan.li <haoyan.li@techshare.co.jp>
2025-09-26 18:05:06 +0900

    Remove obsolete files and add new URDF/XACRO for dual arm base description
    - Deleted the old base_link.STL mesh file.
    - Removed the outdated dual_arm_base.trans transmission file.
    - Introduced a new dual_arm_base.urdf.xacro file containing the complete robot description, including links, joints, and visual/collision geometries.

--- assets/dual_arm_base_description/setup.py
+++ assets/dual_arm_base_description/setup.py
@@ -11,7 +11,7 @@ setup(
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
-        (os.path.join("share", package_name, "launch"), glob("launch/*.launch.py")),
+        (os.path.join("share", package_name, "launch"), glob("launch/*")),
         (os.path.join("share", package_name, "urdf"), glob("urdf/*")),
         (os.path.join("share", package_name, "meshes"), glob("meshes/*")),
         (os.path.join("share", package_name, "config"), glob("config/*")),
@@ -20,7 +20,7 @@ setup(
     zip_safe=True,
     maintainer="root",
     maintainer_email="root@todo.todo",
-    description="TODO: Package description",
+    description=f"The {package_name} package",
     license="MIT",
     tests_require=["pytest"],
     entry_points={
